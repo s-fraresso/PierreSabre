db68105ad678c011abb31c7ff0396601b8a5794d
Sylvain Fraresso <frs4938a@ensfsi.onmicrosoft.com>
2024-11-18 16:22:54 +0100

    Les humains ont des connaissances

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -1,9 +1,13 @@
 package personnages;
 
-public class Humain {
+public class Humain {	
+	static final int NB_CONNAISSANCES_MAX = 30;
+	
 	private String nom;
 	private String boissonFavorite;
 	private int argent;
+	protected int nbConnaissances = 0;
+	protected Humain[] memoire = new Humain[NB_CONNAISSANCES_MAX];
 	
 	
 		
@@ -49,6 +53,55 @@ public class Humain {
 	
 	protected void perdreArgent(int perte) {
 		argent -= perte;
+	}
+	
+	private void oublier(int nbOublis) {
+		assert nbOublis <= nbConnaissances;
+		for (int i = 0; i < nbOublis; ++i) {
+			memoire[i] = null;
+		}
+		for (int j = nbOublis; j < nbConnaissances; ++j) {
+			memoire[j - nbOublis] = memoire[j];
+			memoire[j] = null;
+		}
+		nbConnaissances -= nbOublis;
+	}
+	
+	private void memoriser(Humain autreHumain) {
+		if (nbConnaissances < NB_CONNAISSANCES_MAX) {
+			memoire[nbConnaissances++] = autreHumain;
+		}
+		else {
+			oublier(1);
+			memoriser(autreHumain);
+		}
+	}
+	
+	public void faireConnaissanceAvec(Humain autreHumain) {
+		direBonjour();
+		memoriser(autreHumain);
+		autreHumain.repondre(this);
+	}
+	
+	private void repondre(Humain autreHumain) {
+		direBonjour();
+		memoriser(autreHumain);
+	}
+	
+	public void listerConnaissance() {
+		if (nbConnaissances == 0) {
+			parler("Je ne connais personne...");
+		}
+		else {
+			StringBuilder bld = new StringBuilder();
+			for (int i = 0; i < nbConnaissances; ++i) {
+				bld.append(memoire[i].getNom());
+				if (i < nbConnaissances - 1) {
+					bld.append(", ");
+				}
+			}
+			parler("Je connais beaucoup de monde dont : " + bld.toString());
+		}
 	}
 	
 }
